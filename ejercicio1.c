8045fb2ef7cd68e12f99a02ecd8baf0f158d17f5
Diego Fernandez <diego@diegonotbuc>
2015-09-08 20:11:43 -0300

    ahora sabe quien es el hijo y quien el padre

--- ejercicio1.c
+++ ejercicio1.c
@@ -1,9 +1,19 @@
 #include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
 int main(void){
 	int a, b, c;
+	pid_t hijo;
 	a=1;
 	b=3;
 	c=a+b;
-	printf("Hola mundo! %d\n", c);
+	hijo = fork();
+	if (hijo == 0){
+		printf("soy el hijo de %d\n", getppid());
+	}
+	else{
+		printf("soy el padre de %d\n", hijo);
+	}
+	//printf("Hola mundo! soy %d\n", hijo);
 	return 0;
 }
