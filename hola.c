d1ee6d2d64c3f627bc51e9802784930e0ddb1255
Diego Fernandez <diego@diegonotbuc>
2015-09-11 12:18:29 -0300

    agregado hola.c

---
+++ hola.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+int main (void){
+	pid_t hijo;
+	hijo = fork();
+	if (hijo < 0){
+		printf("Algo salio mal, un culo va a sangrar\n");
+		
+	}
+	if (hijo == 0){
+		printf("Soy el hijo de %d, mi identidad es %d\n", getppid(), getpid());
+		//execlp("/bin/ps","ps",NULL);
+	}
+	else {
+		wait(NULL);
+		printf("Yo soy %d, mi hijo es %d\n", getpid(), hijo);
+		//execlp("/bin/ls", "ls", "-l", NULL);
+	}
+	printf("Hola mundo\n");
+	return 0;
+}
