0cca954bb0783366a9138267c2752e21a91bf241
Diego Fernandez <diegofernandezhunko@outlook.com.ar>
2015-09-17 18:52:31 -0300

    algo.c

--- TP_1/algo.c
+++ TP_1/algo.c
@@ -7,15 +7,15 @@
 
 
 int main (void){
-int i;
-pid_t pid1, pid2;
-for(i = 0; i < 2; i++){
-pid1 = fork();
-pid2 = fork();
-if (pid1 > 0)
-printf("1\n");
-if (pid2 == 0)
-printf("2\n");
-}
-exit(0);
+        int i;
+        pid_t pid1, pid2;
+        for(i = 0; i < 2; i++){
+                pid1 = fork();
+                pid2 = fork();
+                if (pid1 > 0)
+                        printf("1\n");
+                if (pid2 == 0)
+                        printf("2\n");
+        }
+        exit(0);
 }
