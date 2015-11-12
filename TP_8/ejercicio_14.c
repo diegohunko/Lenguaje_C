5274e2eb95035a9b144cc4f685ae121f2e73f45e
Diego Fernandez <diegofernandezhunko@outlook.com.ar>
2015-11-12 20:20:01 -0300

    ejercicio 14

---
+++ TP_8/ejercicio_14.c
@@ -0,0 +1,38 @@
+/*
+ * ejercicio_14.c
+ * 
+ * Copyright 2015 Diego Fernandez <diego@diegonotbuc>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+/*14) Desarrolle un programa sv con una funcionalidad semejante al comando cp de Unix.
+ * Su sintaxis es la siguiente: sv <f1> <f2> ...<fn> directorio
+ * El comando sv copia los ficheros al directorio directorio siempre que sean más recientes
+ * que las versiones existentes en directorio.
+ * 
+ */
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	
+	return 0;
+}
+
